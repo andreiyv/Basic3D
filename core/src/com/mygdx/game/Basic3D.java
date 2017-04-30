7b60cd44f1680948a1032245281b20bf19102548
Andrey <voran@inbox.ru>
2017-04-30 09:46:33 +0300

    update

--- core/src/com/mygdx/game/Basic3D.java
+++ core/src/com/mygdx/game/Basic3D.java
@@ -6,11 +6,13 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g3d.Environment;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
@@ -19,13 +21,20 @@ import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
     public class Basic3D implements ApplicationListener {
         public PerspectiveCamera cam;
         public ModelBatch modelBatch;
-        public Model model;
-        public ModelInstance instance;
+        public Model model1, model2;
+        public ModelInstance instance1, instance2;
         public CameraInputController camController;
+        protected Environment environment;
 
         @Override
         public void create() {
             modelBatch = new ModelBatch();
+
+            modelBatch = new ModelBatch();
+            environment = new Environment();
+            environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+            environment.add(new DirectionalLight().set(0.8f, 15f, 0.8f, -1f, -0.8f, -0.2f));
+            environment.add(new DirectionalLight().set(15f, 0.8f, 0.8f, 0.01f, -0.1f, -0.1f));
 
             cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
             cam.position.set(10f, 10f, 10f);
@@ -35,10 +44,16 @@ import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
             cam.update();
 
             ModelBuilder modelBuilder = new ModelBuilder();
-            model = modelBuilder.createBox(5f, 5f, 5f,
+            model1 = modelBuilder.createBox(5f, 5f, 5f,
                     new Material(ColorAttribute.createDiffuse(Color.GREEN)),
                     Usage.Position | Usage.Normal);
-            instance = new ModelInstance(model);
+
+            model2 = modelBuilder.createCone(5f, 15f, 5f,10,
+                    new Material(ColorAttribute.createDiffuse(Color.BLUE)),
+                    Usage.Position | Usage.Normal);
+
+            instance1 = new ModelInstance(model1);
+            instance2 = new ModelInstance(model2);
 
             camController = new CameraInputController(cam);
             Gdx.input.setInputProcessor(camController);
@@ -50,14 +65,16 @@ import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
             Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
             modelBatch.begin(cam);
-            modelBatch.render(instance);
+            modelBatch.render(instance1, environment);
+            modelBatch.render(instance2, environment);
             modelBatch.end();
         }
 
         @Override
         public void dispose() {
             modelBatch.dispose();
-            model.dispose();
+            model1.dispose();
+            model2.dispose();
         }
 
         @Override
