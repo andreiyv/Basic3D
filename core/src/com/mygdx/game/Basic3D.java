b06e6b594dbb2ab42f15aae7e7217bcb6263cc48
Andrey <voran@inbox.ru>
2017-05-01 19:25:26 +0300

    update

--- core/src/com/mygdx/game/Basic3D.java
+++ core/src/com/mygdx/game/Basic3D.java
@@ -1,5 +1,6 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
@@ -13,78 +14,106 @@ import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.model.Node;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.utils.MeshPartBuilder;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
 
+public class Basic3D implements ApplicationListener {
+    public PerspectiveCamera cam;
+    public ModelBatch modelBatch;
+    public Model model1, model2;
+    public ModelInstance instance1, instance2;
+    public CameraInputController camController;
+    public Environment environment;
+    public Model cube, cone, sphere, table;
+    public Material mat_cube, mat_cone, mat_sphere, mat_table, mat_ground;
+    public ModelInstance inst_cone, inst_building, inst_ground;
 
-    public class Basic3D implements ApplicationListener {
-        public PerspectiveCamera cam;
-        public ModelBatch modelBatch;
-        public Model model1, model2;
-        public ModelInstance instance1, instance2;
-        public CameraInputController camController;
-        protected Environment environment;
+    @Override
+    public void create() {
+        modelBatch = new ModelBatch();
+        mat_cube = new Material(ColorAttribute.createDiffuse(Color.GREEN));
+        mat_cone = new Material(ColorAttribute.createDiffuse(Color.GREEN));
+        mat_sphere = new Material(ColorAttribute.createDiffuse(Color.BLUE));
+        mat_table = new Material(ColorAttribute.createDiffuse(Color.WHITE));
+        mat_ground = new Material(ColorAttribute.createDiffuse(Color.GRAY));
 
-        @Override
-        public void create() {
-            modelBatch = new ModelBatch();
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
 
-            modelBatch = new ModelBatch();
-            environment = new Environment();
-            environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
-            environment.add(new DirectionalLight().set(0.8f, 15f, 0.8f, -1f, -0.8f, -0.2f));
-            environment.add(new DirectionalLight().set(15f, 0.8f, 0.8f, 0.01f, -0.1f, -0.1f));
 
-            cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-            cam.position.set(10f, 10f, 10f);
-            cam.lookAt(0,0,0);
-            cam.near = 1f;
-            cam.far = 300f;
-            cam.update();
+        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.position.set(10f, 10f, 10f);
+        cam.lookAt(0, 0, 0);
+        cam.near = 1f;
+        cam.far = 300f;
+        cam.update();
 
-            ModelBuilder modelBuilder = new ModelBuilder();
-            model1 = modelBuilder.createBox(5f, 5f, 5f,
-                    new Material(ColorAttribute.createDiffuse(Color.GREEN)),
-                    Usage.Position | Usage.Normal);
+        ModelBuilder modelBuilder = new ModelBuilder();
 
-            model2 = modelBuilder.createCone(5f, 15f, 5f,10,
-                    new Material(ColorAttribute.createDiffuse(Color.BLUE)),
-                    Usage.Position | Usage.Normal);
+        modelBuilder.begin();
 
-            instance1 = new ModelInstance(model1);
-            instance2 = new ModelInstance(model2);
+        Node node1 = modelBuilder.node();
+        node1.id = "node1";
+        node1.translation.set(0f, 2.8f, 0f);
+        MeshPartBuilder meshBuilder;
+        meshBuilder = modelBuilder.part("part1", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, mat_ground);
+        meshBuilder.box(9f, 0.1f, 9f);
 
-            camController = new CameraInputController(cam);
-            Gdx.input.setInputProcessor(camController);
-        }
+        Model ground = modelBuilder.end();
 
-        @Override
-        public void render() {
-            Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        modelBuilder.begin();
 
-            modelBatch.begin(cam);
-            modelBatch.render(instance1, environment);
-            modelBatch.render(instance2, environment);
-            modelBatch.end();
-        }
+            node1 = modelBuilder.node();
+            node1.id = "node1";
+            node1.translation.set(0f, 2.8f, 0f);
+ //               MeshPartBuilder meshBuilder;
+                meshBuilder = modelBuilder.part("part1", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, mat_cone);
+                meshBuilder.box(1, 5, 1);
 
-        @Override
-        public void dispose() {
-            modelBatch.dispose();
-            model1.dispose();
-            model2.dispose();
-        }
+        Model building = modelBuilder.end();
 
-        @Override
-        public void resize(int width, int height) {
-        }
 
-        @Override
-        public void pause() {
-        }
 
-        @Override
-        public void resume() {
-        }}
+        inst_ground = new ModelInstance(ground);
+        inst_building = new ModelInstance(building);
+
+        inst_building.transform.scl(0.5f);
+
+        camController = new CameraInputController(cam);
+        Gdx.input.setInputProcessor(camController);
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        modelBatch.begin(cam);
+        modelBatch.render(inst_building, environment);
+        modelBatch.render(inst_ground, environment);
+        modelBatch.end();
+    }
+
+    @Override
+    public void dispose() {
+        modelBatch.dispose();
+        model1.dispose();
+        model2.dispose();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+}
