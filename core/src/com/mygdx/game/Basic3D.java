63b27b38e9f635d2c6b92092e866ac15eb3d3f57
Andrey Voronin <andrey.voronin@cd-adapco.com>
2017-05-04 15:15:29 +0300

    Update

--- core/src/com/mygdx/game/Basic3D.java
+++ core/src/com/mygdx/game/Basic3D.java
@@ -6,14 +6,19 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g3d.Environment;
 import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.MeshPartBuilder;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 
 
 public class Basic3D implements ApplicationListener {
@@ -22,9 +27,21 @@ public class Basic3D implements ApplicationListener {
     public Model model;
     public ModelInstance instance, instance1;
     public CameraInputController camController;
+    protected Stage stage;
+    protected Label label;
+    protected BitmapFont font;
+    protected StringBuilder stringBuilder;
+    public Environment environment;
 
     @Override
     public void create() {
+
+        stage = new Stage();
+        font = new BitmapFont();
+        label = new Label(" ", new Label.LabelStyle(font, Color.WHITE));
+        stage.addActor(label);
+        stringBuilder = new StringBuilder();
+
         modelBatch = new ModelBatch();
 /*
 
@@ -38,6 +55,10 @@ public class Basic3D implements ApplicationListener {
         cam.near = 1f;
         cam.far = 300f;
         cam.update();
+
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.1f, 0.1f, 0.1f, 1f));
+        environment.add(new DirectionalLight().set(1.8f, 1.8f, 1.8f, -1f, -0.8f, -0.2f));
 
         camController = new CameraInputController(cam);
         Gdx.input.setInputProcessor(camController);
@@ -78,9 +99,19 @@ public class Basic3D implements ApplicationListener {
         Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
+        camController.update();
+
         modelBatch.begin(cam);
-        modelBatch.render(instance);
+        modelBatch.render(instance, environment);
         modelBatch.end();
+
+        stringBuilder.setLength(0);
+        stringBuilder.append(" FPS: ").append(Gdx.graphics.getFramesPerSecond());
+        stringBuilder.append(" Visible: ").append(visibleCount);
+        stringBuilder.append(" Selected: ").append(selected);
+        label.setText(stringBuilder);
+        stage.draw();
+
     }
 
     @Override
